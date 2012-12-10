5a27dcece5378b164b8c91fab7f1479da5df08e5
Liakoni <liakoni@gmail.com>
2012-12-10 21:46:52 +0200

    Adding Files

---
+++ README.txt
@@ -0,0 +1,74 @@
+Converts multiple pgn files to csv structure.
+Currently(v0.6) works only in unix platforms due to ('/') directory trailing slashes. Windows version easy to supply if needed.
+
+Usage : python pgn2csv.py -d <root_directory_where_to_start_looking_for_pgns> -o <output_directory> -i <id_starting_point>
+or    : python pgn2csv.py -f <lonely_pgn_path> -o <output_directory> -i <id_starting_point>
+help  : python pgn2csv.py --help
+
+All arguments are optional. If neither is specified then pgn2csv runs in directory mode where it considers as source directory (-d) the current directory(the one where script is executed) and as output directory (the same). The id default initial value is 1.
+
+So the easiest way to execute is to put pgn2csv.py in the top folder( where the pgn files are ) in your disk and just run:
+>> python pgn2csv.py
+
+Since Version 0.6 and later, pgn2csv recursively finds all pgns from -d subfolders
+
+
+=== CHANGES in v1.4 ===
+
+Since this version, an additional column is added before moves, called 'Title' (this is convinient when importing nodes in drupal).
+This is autofilled with the value [White] - [Black], aka: the names with a dash in the middle.
+
+Also since now the id default value is 1 instead of 0 which was what v1.2 introduced when supposed to fix that lol 
+
+
+=== CHANGES in v1.3 ===
+
+Guess what. Just fixed another idiotic bug.
+Moves wasn't printing in the exact corresponding column, instead it was printing one empty column after the last tag. (aka:
+We had an empty column in the values : , , 
+
+
+=== CHANGES in v1.2 ===
+
+Fixed the following bug:
+counter (id) specified by argument -i was incremented before writed so we had a +1 initial starting point in the id column
+
+
+=== CHANGES in v1.1 ===
+
+Critical bug fixed when occuring EventDate tag, script was executing the part of the code belonging to 'Event' tag...
+bakery style fix for the time being by making the condition statement 
+if line[0:7] == '[Event ': 
+
+(trolol) y i know
+
+
+=== CHANGES in v1.0 ===
+
+Id counter can now be initialized from command line (-i , --id)-argument
+Since now script doesn't make use of the odict.py file, it imports the apporpirate library instead
+
+
+=== CHANGES in v0.9 ===
+
+Completely rewritten(process_file func) so that now we identify the tags readed, storing them in an ordered Dictionary and then
+writting to file (csv) with the appropriate order. It ignores all the unidentified tags (see below the list with tags searching).
+
+By that way we can assume that since now png2csv convertor car read ANY type of pgn and produce a similar(same columns) csv file.
+
+The standard column(tags) are now:
+Id, Event, Site, Date, Round, White, Black, Result, ECO, WhiteTitle, WhiteElo, WhiteFideId, BlackTitle, BlackElo, BlackFideId, EventDate, Opening, Variation, Moves
+
+
+=== CHANGES in v0.8 ===
+
+1. Completely different logic of detecting tags. Since now we read the 7 standard tags (by wikipedia pgn specification : http://en.wikipedia.org/wiki/Portable_Game_Notation#Tag_pairs ) and IGNORE all the others below until the moves section
+
+2. Added an Id column before 'Event' needed by the drupal's module 
+
+
+=== CHANGES in v0.7 ===
+
+Since Version 0.7 and later, pgn2csv surrounds moves section(cell) with [pgn] , [/pgn] tags used by : http://code.google.com/p/pgn4web/wiki/User_Notes_drupal
+
+
---
+++ pgn2csv.py
@@ -0,0 +1,143 @@
+#pgn2csv is free software: you can redistribute it
+#and/or modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation, either version 3
+#of the License, or (at your option) any later version.
+
+#You should have received a copy of the GNU General Public License
+#along with pgn2csv.  If not, see <http://www.gnu.org/licenses/>.
+
+#Copyleft 2012 - Author: chefarov@gmail.com
+#version 1.1
+
+import sys
+import os
+import argparse
+from collections import OrderedDict
+
+default_dir = os.getcwd()
+
+'''process pgn's lines, seperating tag's from their values and removing characters like quotes commas etc'''
+def process_line(line, tags):
+    tag,  value = line.split(' ',  1)            #split each line to its 1st whitespace (2nd arg means: 1 split only)             
+    tag = tag[1:]                                        #remove '[' (1st character)
+    value = value.replace( ',' ,  '' )       #name fields may contain name and lastname seperated by comma (remove it to keep csv fields intact)
+    value = value.rstrip( '\r\n' )             #remove newline chars
+    if tags.has_key(tag):                           #do not add arbitrary tags
+        tags[tag] = value[1:-2]                   #also remove last two chars : "] and the 1st one : "
+
+def write_to_file(tags, fout):
+    global id
+    for v in tags.values():
+        fout.write(str(v)+', ') 
+    
+def initDic(dict):
+    for key in dict.keys():
+        dict[key] = ' '
+
+'''sourceFile: the path of source file (pgn)  --- outputDir: output directory for (csv files)'''
+def process_file(sourceFile,  outputDir=default_dir):    
+    global id
+    #Creating the output Directory
+    if os.path.exists(outputDir) == False:    #if directory doesn't exist create it
+        os.makedirs(outputDir)                       #also creates intermediate directories
+    #Opening files
+    sourceFileDirs = sourceFile.split('/')    #in case an absolute path is provided
+    sourceFileName = sourceFileDirs[-1]     #take the last part of the path which is the file's name
+    foutName = outputDir+'/'+sourceFileName[:-3] + "csv"  
+    try:
+        fin = open(sourceFile, 'r')
+        fout = open(foutName,  'w')
+
+        #Reading - Writing files
+        fout.write('Id, Event, Site, Date, Round, White, Black, Result, ECO, WhiteTitle, WhiteElo, WhiteFideId, '+ 
+                          'BlackTitle, BlackElo, BlackFideId, EventDate, Opening, Variation, Title, Moves')
+        initItems = [('Id', ' '), ('Event',' '), ('Site',' '), ('Date',' '), ('Round',' '), ('White',' ') , ('Black',' '), 
+                           ('Result',' '), ('ECO',' '),  ('WhiteTitle', ' '), ('WhiteElo', ' ') , ('WhiteFideId',' '),  ('BlackTitle', ' '), 
+                           ('BlackElo',' '), ('BlackFideId',' ') , ('EventDate', ' '),  ('Opening', ' '), ('Variation',' ')]
+        tags = OrderedDict(initItems)         #ordered Dictionary creation
+    
+        flag = True                           #helping flag to apply [pgn] , [/pgn] pgn4web flags only once for every game in moves section
+        firstLineFlag = True              #helping flag to not apply /pgn tag in 1st line
+
+        for line in fin:
+            if line[0:7] == '[Event ':       #previous line/row/entry/game is over go on (one pgn can contain multiple games)
+               #reaching here means line contains event info
+                if firstLineFlag == False:         #every time we come up with a new game except the 1st time
+                    fout.write(' [/pgn]')         #close the pgn4web tag
+                firstLineFlag = False
+                flag = True
+                initDic(tags)                            #empty dictionary from previous game's values
+                tags['Id'] = id
+                id = id + 1
+                fout.write('\n') 
+                process_line(line, tags)           #now we are ready to write the tag's value like we do in every tag
+            elif line[0].isdigit():                               #moves section
+             
+                write_to_file(tags, fout)       #move the tags' values from dictionary to file before writing moves 
+                 #before the moves append the white-black info (not in the tags) - feature helping drupal site :P
+                fout.write(tags['White']+' - '+tags['Black']+', ')
+                
+                while line not in ['\n', '\r\n'] :     #read all the lines containing moves
+                    if flag:                                        #apply tag only before 1st move in each game
+                        fout.write('[pgn] ')             #apply tags for pgn4web automation board presentation software
+                        flag = False                            #do not apply tag after every newline(from pgn file) with moves
+                    a = line.rstrip('\r\n')        #remove newline character and '\r' $MS$ b00l$h1t
+                    fout.write( a+' ' )              #write all the moves in one cell
+                    line = fin.next()                  #read next line
+                    if len(line) == 0:                 #trying to catch EOF but never yet - StopIteration exception is raised and handled below
+                        break
+            elif len(line) > 2 :                      #not empty remember \r\n make len(line) == 2 
+                process_line(line, tags)          #ordinary tag, write its value to dictionary(tags) 
+        #end of external for loop
+            
+        fout.write('[/pgn]')                   #last tag outside the loop
+        #Closing the files
+        fin.close()
+        fout.close()
+    except StopIteration:
+        fout.write('[/pgn]')        #because when there is not an empty line at the End Of File we get that exception in line 76: line=fin.next()
+        fout.close()
+        fin.close()
+    except IOError:
+        print "Sth wrong with Input file: ", sourceFile, "or output directory: ", outputDir
+        fout.close()
+        fin.close()
+
+'''sourceDir: the path of the directory containing src files --- outputDir: output directory for (csv files)'''
+def process_dir(sourceDir=default_dir, outputDir=default_dir):    
+
+    for x in os.listdir(sourceDir):
+        if x == "csvFiles":
+            continue
+        path = sourceDir + '/' + x
+        if os.path.isdir(path):                  # directory - recursive call
+            folderPaths = path.split('/')   # not the folderName yet ... just splitting the path
+            folderName = str(folderPaths[-1])
+            if folderName == "csvFiles":
+                continue
+            outDir = outputDir +'/'+ folderName
+            process_dir(path, outDir    )       #recursive call to the new path but output Directory is kept to outDir
+        elif path[-4:] == '.pgn':         #if we find a pgn file then we call the process_file func
+            process_file(path, outputDir)
+
+
+if __name__ == "__main__":
+    global id             #counter for the 1st column of csv
+    parser = argparse.ArgumentParser(description='usage: >>python -f file or >>python -d directory')    
+    parser.add_argument('-f', '--file',  help='path of the pgn file')
+    parser.add_argument('-d', '--directory',  help='path of the pgn directory(multiple source files)-(default: current directory', default=default_dir)
+    parser.add_argument('-o', '--outputdir',  help='path of output directory (default: current directory)',  default=default_dir)
+    parser.add_argument('-i', '--id',  help='starting id counter (default = 1)',  default=1)
+    args = parser.parse_args()
+    
+    id = int(args.id)
+    if args.file == None:  #no specific file specified
+        outDir = args.outputdir + '/' + 'csvFiles'
+        if os.path.exists(outDir) == False:    #if directory doesn't exist create it
+            os.mkdir(outDir)
+        process_dir(args.directory,  outDir )       #work with directory
+    else:
+        process_file(args.file,  args.outputdir)    #work with file
+
+    print "Conversion completed successfully"
+
